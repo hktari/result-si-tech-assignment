36e73a349aa500d17be7dee553fe820f2e86c9d9
hktari <bkamnik1995@gmail.com>
2025-07-18 11:32:18 +0200

    set cors origin to '*'

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   // Enable CORS
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: '*',
     credentials: true,
   });
 
